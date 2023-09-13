e51973f3510ea9cfc77a5dabe417dbfa7c6a201e
Nate Baer <njbbaer@gmail.com>
2023-09-12 23:52:18 -0700

    Allow setting context file name

--- src/context.py
+++ src/context.py
@@ -3,46 +3,45 @@ from ruamel.yaml.scalarstring import LiteralScalarString
 
 
 class Context:
-    def __init__(self, config_file):
-        with open(config_file, 'r') as file:
-            self.dict = yaml.load(file)
+    def __init__(self, context_file):
+        self.context_file = context_file
         self.save()
 
     def save(self):
-        with open('context.yml', 'w') as file:
-            yaml.dump(self.dict, file)
+        with open(self.context_file, 'w') as file:
+            yaml.dump(self.data, file)
 
     def reload(self):
-        with open('context.yml', 'r') as file:
-            self.dict = yaml.load(file)
+        with open(self.context_file, 'r') as file:
+            self.data = yaml.load(file)
 
     def add_message(self, role, message):
-        self.dict.setdefault('messages', []).append({
+        self.data.setdefault('messages', []).append({
             'role': role,
             'content': LiteralScalarString(message)
         })
         self.save()
 
     def set_memory(self, memory):
-        self.dict['current_memory'] = memory
+        self.data['current_memory'] = memory
         self.save()
 
     def clear_messages(self):
-        del self.dict['messages']
+        del self.data['messages']
         self.save()
 
     @property
     def chat_messages(self):
-        return self.dict.get('messages') or []
+        return self.data.get('messages') or []
 
     @property
     def chat_prompt(self):
-        return LiteralScalarString(self.dict['chat_prompt'] + '\n\n' + self.current_memory)
+        return LiteralScalarString(self.data['chat_prompt'] + '\n\n' + self.current_memory)
 
     @property
     def memorizer_prompt(self):
-        return self.dict['memory_prompt']
+        return self.data['memory_prompt']
 
     @property
     def current_memory(self):
-        return self.dict['current_memory']
+        return self.data['current_memory']

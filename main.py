71c488047c2d4509a80791897d2c0ae5493fe27e
Nate Baer <njbbaer@gmail.com>
2023-09-14 17:11:37 -0700

    Refactor telegram bot code into a class

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 import argparse
 
-from src.telegram_bot import configure_bot
+from src.telegram_bot import TelegramBot
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('context_file', type=str)
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    bot = configure_bot()
-    bot.infinity_polling()
+    args = get_args()
+    TelegramBot(args.context_file).start()
--- src/telegram_bot.py
+++ src/telegram_bot.py
@@ -1,87 +1,76 @@
 import os
-import argparse
 import telebot
 from dotenv import load_dotenv
 import threading
 import time
 from contextlib import contextmanager
 
-
 from src.simulacrum import Simulacrum
+
 
 load_dotenv()
 
 
-def configure_bot():
-    args = get_args()
-    sim = Simulacrum(args.context_file)
-    bot = telebot.TeleBot(os.environ['API_TOKEN'])
+class TelegramBot:
+    def __init__(self, context_file):
+        self.sim = Simulacrum(context_file)
+        self.bot = telebot.TeleBot(os.environ['API_TOKEN'])
+        self._configure_handlers()
 
-    configure_handlers(bot, sim)
-    return bot
+    def start(self):
+        self.bot.infinity_polling()
 
+    def _configure_handlers(self):
+        # Must be first to catch unauthorized messages
+        @self.bot.message_handler(func=self.is_unauthorized)
+        def unauthorized_message_handler(message):
+            self._send_message(message.chat.id, '🚫 Unauthorized', is_block=True)
 
-def configure_handlers(bot, sim):
-    # Must be first to catch unauthorized messages
-    @bot.message_handler(func=is_unauthorized)
-    def unauthorized_message_handler(message):
-        send_message(bot, message.chat.id, '🚫 Unauthorized', is_block=True)
+        @self.bot.message_handler(commands=['integrate'])
+        def integrate_command_handler(message):
+            with self._process_with_feedback(message.chat.id):
+                self.sim.integrate_memory()
+                self._send_message(message.chat.id, '🧠 Memory integration complete', is_block=True)
 
-    @bot.message_handler(commands=['integrate'])
-    def integrate_command_handler(message):
-        with process_with_feedback(bot, message.chat.id):
-            sim.integrate_memory()
-            send_message(bot, message.chat.id, '🧠 Memory integration complete', is_block=True)
+        @self.bot.message_handler(commands=['retry'])
+        def retry_command_handler(message):
+            with self._process_with_feedback(message.chat.id):
+                response = self.sim.retry()
+                self._send_message(message.chat.id, response)
 
-    @bot.message_handler(commands=['retry'])
-    def retry_command_handler(message):
-        with process_with_feedback(bot, message.chat.id):
-            response = sim.retry()
-            send_message(bot, message.chat.id, response)
+        @self.bot.message_handler()
+        def message_handler(message):
+            with self._process_with_feedback(message.chat.id):
+                response = self.sim.chat(message.text)
+                self._send_message(message.chat.id, response)
 
-    @bot.message_handler()
-    def message_handler(message):
-        with process_with_feedback(bot, message.chat.id):
-            response = sim.chat(message.text)
-            send_message(bot, message.chat.id, response)
+    def _send_message(self, chat_id, message, is_block=False):
+        message = f'```\n{message}\n```' if is_block else message
+        self.bot.send_message(chat_id, message, parse_mode='Markdown')
 
+    def is_unauthorized(self, message):
+        return str(message.chat.id) != os.environ['USER_ID']
 
-def get_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('context_file', type=str)
-    return parser.parse_args()
+    @contextmanager
+    def _process_with_feedback(self, chat_id):
+        try:
+            with self._show_typing(chat_id):
+                yield
+        except Exception as e:
+            error_text = f'❌ An error occurred: {str(e)}'
+            self._send_message(chat_id, error_text, is_block=True)
 
+    @contextmanager
+    def _show_typing(self, chat_id):
+        stop_typing = threading.Event()
 
-def send_message(bot, chat_id, message, is_block=False):
-    message = f'```\n{message}\n```' if is_block else message
-    bot.send_message(chat_id, message, parse_mode='Markdown')
+        def send_typing_periodically():
+            while not stop_typing.is_set():
+                self.bot.send_chat_action(chat_id, action='typing')
+                time.sleep(4)
 
-
-def is_unauthorized(message):
-    return str(message.chat.id) != os.environ['USER_ID']
-
-
-@contextmanager
-def process_with_feedback(bot, chat_id):
-    try:
-        with show_typing(bot, chat_id):
-            yield
-    except Exception as e:
-        error_text = f'❌ An error occurred: {str(e)}'
-        send_message(bot, chat_id, error_text, is_block=True)
-
-
-@contextmanager
-def show_typing(bot, chat_id):
-    stop_typing = threading.Event()
-
-    def send_typing_periodically():
-        while not stop_typing.is_set():
-            bot.send_chat_action(chat_id, action='typing')
-            time.sleep(4)
-
-    thread = threading.Thread(target=send_typing_periodically)
-    thread.start()
-    yield
-    stop_typing.set()
-    thread.join()
+        thread = threading.Thread(target=send_typing_periodically)
+        thread.start()
+        yield
+        stop_typing.set()
+        thread.join()

0edde8e61230d46ea6a1dea14ca595fe1c812baa
Nate Baer <njbbaer@gmail.com>
2024-01-13 12:44:37 -0800

    Remove unused code

--- src/executors/executor.py
+++ src/executors/executor.py
@@ -14,15 +14,9 @@ class Executor(ABC):
     async def _generate_chat_completion(self, messages, parameters):
         completion = await ChatCompletion.generate(messages, parameters)
         self.context.add_cost(completion.cost)
-        self._set_stats(completion)
         return completion
 
     async def _generate_legacy_completion(self, prompt, parameters):
         completion = await LegacyCompletion.generate(prompt, parameters)
         self.context.add_cost(completion.cost)
         return completion
-
-    def _set_stats(self, completion):
-        self.context.last_cost = completion.cost
-        self.context.last_prompt_tokens = completion.prompt_tokens
-        self.context.last_completion_tokens = completion.completion_tokens

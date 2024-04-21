542e028b3c534896fe48832d9c18677d94b030d3
Nate Baer <njbbaer@gmail.com>
2024-04-21 06:00:41 -0700

    Create merge lists jinja helper

--- src/template_filters.py
+++ src/template_filters.py
@@ -9,5 +9,10 @@ def _shuffle_list(list):
     return shuffled_list
 
 
+def _merge_lists(list1, list2):
+    return list1 + list2
+
+
 def register_filters():
     jinja2.filters.FILTERS["shuffle"] = _shuffle_list
+    jinja2.filters.FILTERS["merge"] = _merge_lists

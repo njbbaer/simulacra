9302044b16cf1cf1877de9afba5ae93a1ecfbce2
Nate Baer <njbbaer@gmail.com>
2024-08-15 23:57:49 -0700

    Resolve file references recursively

--- src/resolve_vars.py
+++ src/resolve_vars.py
@@ -4,43 +4,33 @@ import jinja2
 
 
 def resolve_vars(vars, base_path):
-    vars = _dereference_vars(vars, base_path)
-    return _render_vars(vars)
-
-
-def _dereference_vars(vars, base_path):
-    dereferenced_vars = vars.copy()
-    for key, value in vars.items():
-        if isinstance(value, str) and value.startswith("file:"):
-            file_path = value.split("file:", 1)[1]
-            full_path = os.path.join(base_path, file_path)
-            with open(full_path) as file:
-                dereferenced_vars[key] = file.read()
-    return dereferenced_vars
-
-
-def _render_vars(vars):
     MAX_ITERATIONS = 10
 
     for _ in range(MAX_ITERATIONS):
-        rendered_vars = _render_vars_recursive(vars)
-        if rendered_vars == vars:
-            break
-        vars = rendered_vars
-    else:
-        raise RuntimeError("Too many iterations resolving vars. Circular reference?")
-    return rendered_vars
+        resolved_vars = _resolve_vars_recursive(vars, base_path, vars)
+        if resolved_vars == vars:
+            return resolved_vars
+        vars = resolved_vars
+
+    raise RuntimeError("Too many iterations resolving vars. Circular reference?")
 
 
-def _render_vars_recursive(vars, obj=None):
-    if obj is None:
-        obj = vars
-
+def _resolve_vars_recursive(obj, base_path, context):
     if isinstance(obj, dict):
-        return {key: _render_vars_recursive(vars, value) for key, value in obj.items()}
+        return {
+            key: _resolve_vars_recursive(value, base_path, context)
+            for key, value in obj.items()
+        }
     elif isinstance(obj, list):
-        return [_render_vars_recursive(vars, item) for item in obj]
+        return [_resolve_vars_recursive(item, base_path, context) for item in obj]
     elif isinstance(obj, str):
-        return jinja2.Template(obj, trim_blocks=True, lstrip_blocks=True).render(vars)
+        if obj.startswith("file:"):
+            file_path = obj.split("file:", 1)[1]
+            full_path = os.path.join(base_path, file_path)
+            with open(full_path) as file:
+                return file.read()
+        return jinja2.Template(obj, trim_blocks=True, lstrip_blocks=True).render(
+            context
+        )
     else:
         return obj

8726ec234142412305deed5b2d5f065fcefb67be
Nate Baer <njbbaer@gmail.com>
2026-02-06 22:46:11 -0800

    Don't set base_file_url for local mode

--- src/telegram/telegram_bot.py
+++ src/telegram/telegram_bot.py
@@ -41,11 +41,7 @@ class TelegramBot:
             .concurrent_updates(True)
         )
         if TELEGRAM_BOT_API:
-            builder = (
-                builder.base_url(f"{TELEGRAM_BOT_API}/bot")
-                .base_file_url(f"{TELEGRAM_BOT_API}/file/bot")
-                .local_mode(True)
-            )
+            builder = builder.base_url(f"{TELEGRAM_BOT_API}/bot").local_mode(True)
         self.app = builder.build()
         self.sim = Simulacrum(context_filepath)
         self.cost_tracker = CostTracker()
